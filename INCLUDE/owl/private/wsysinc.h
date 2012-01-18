25cb65bc8af9446539380d8a9958986326dee7fb
Chris Driver <Chris.Driver@taxsystems.com>
2012-01-18 10:36:31 +0000

    build of libraries with _WIN32_WINNT defined as 0x0501 instead of 0x0500

--- INCLUDE/owl/private/wsysinc.h
+++ INCLUDE/owl/private/wsysinc.h
@@ -53,7 +53,8 @@
 
     // define Windows as Versin 5 to get additional data
 #if !defined(_WIN32_WINNT)
-#   define _WIN32_WINNT 0x0500
+#define _WIN32_WINNT 0x0501	// as per current VS version
+//#   define _WIN32_WINNT 0x0500
 //#  define _WIN32_WINNT 0x0400
 #endif
 
